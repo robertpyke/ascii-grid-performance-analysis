2b0990e6173933535eb8631194520df5b31ff7a8
Robert Pyke <robert@robert-VirtualBox.(none)>
2013-04-01 21:59:42 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsciiGridPerformanceAnalysis::Application.config.secret_token = '921bc57402b2353b5377dc633d2e8ac5ca37d41d3a982de1c9af76dfc3f5c358c2f7f895dbc30f9405aa1ddd6ec29db254534f855bb5d4f06757f7fdde0ed9b4'
